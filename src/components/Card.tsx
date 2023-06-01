17103ea7008dc68d5661d13acd305e63598912aa
mykfrost <okellofrost@gmail.com>
2023-06-01 13:50:27 +0300

    creating dynamic lists

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,7 @@
 function Card() {
   return (
     <div className="card">
-      <img src="src/assets/advert.png" className="card-img-top" alt="..." />
+      <img src="../src/assets/advert.png" className="card-img-top" alt="..." />
       <div className="card-body">
         <h5 className="card-title">Card title</h5>
         <p className="card-text">
