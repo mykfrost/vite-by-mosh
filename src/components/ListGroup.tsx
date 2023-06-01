d45231d26ee3a0d758549a9a06f1a5f3e79059e2
mykfrost <okellofrost@gmail.com>
2023-06-01 14:51:30 +0300

    conditionally rendering the empty list using ternary operator

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -9,16 +9,17 @@ function ListGroup() {
     "Tokyo",
   ];
   items = [];
-  if (items.length === 0)
-    return (
-      <Fragment>
-        <h1>List</h1>
-        <p>No Items</p>
-      </Fragment>
-    );
+  // if (items.length === 0)
+  //   return (
+  //     <Fragment>
+  //       <h1>List</h1>
+  //       <p>No Items</p>
+  //     </Fragment>
+  //   );
   return (
     <Fragment>
       <h1>List Group</h1>
+      {items.length === 0 ? <p>No Items</p> : null}
       <ul className="list-group">
         {/* <li className="list-group-item">An item</li>
         <li className="list-group-item">A second item</li>
