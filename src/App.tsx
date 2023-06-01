dda341943d1ea43bc61724622890401e5a6d67ca
mykfrost <okellofrost@gmail.com>
2023-06-01 13:28:01 +0300

    The new react tuitorial using vite

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Message from "./Message";
+import ListGroup from "./components/ListGroup";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <div>
+      {/* <Message/> */}
+      <Card />
+      <ListGroup />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+//Pascal casing 
+function Message(){
+const name = 'Michael Okello'
+
+if(name)
+return <h1>Hello {name}!</h1>
+
+    return <h1>Hello World!</h1>
+}
+export default Message;
